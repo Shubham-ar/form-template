0ab16a85f2dded3829bf572149f88a0af3495c74
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-05-08 19:48:14 +0530

    commit on 'main'

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const path=require('path')
 const port =80;
 
 
+var mongoose= require('mongoose');
+mongoose.connect('mongodb://localhost/userDetails',{useNewUrlParser:true})
+
 app.use('/static', express.static('public'));
 
+var userSchema =new mongoose.Schema({
+  firstName:String,
+  lastName:String,
+  contact:String,
+  emailID:String
+})
+
+var user= mongoose.model('User',userSchema)
 
 
 app.get('/', (req, res)=> {
