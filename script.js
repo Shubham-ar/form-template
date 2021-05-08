719b109c44855351079990ba51ece90aa9e202f4
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-05-08 19:50:55 +0530

    commit on main

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ mongoose.connect('mongodb://localhost/userDetails',{useNewUrlParser:true})
 
 app.use('/static', express.static('public'));
 
+var userSchema =new mongoose.Schema({
+  firstName:String,
+  lastName:String,
+  contact:String,
+  emailID:String
+})
 
 var user= mongoose.model('User',userSchema)
 
