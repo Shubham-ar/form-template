046e1bba29ba33bfe136211704d70667e813f269
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-05-09 18:45:38 +0530

    commit on main

--- script.js
+++ script.js
@@ -18,35 +18,45 @@ var userSchema =new mongoose.Schema({
 //Now we need to create a model of the schema to make insertion and deletion easier
 var User= mongoose.model('User',userSchema);
 
+//to serve static files like css and fotos into the server created. to serve files, one must specify their paths as /static/anthing.css
 app.use('/static', express.static('public'));
-// i was not using this app.use express.urlencoded that's why i was getting null values in post request and req.body
 
+
+// i was not using this app.use express.urlencoded that's why i was getting null values in post request and req.body
 app.use(express.urlencoded())
+
 
 app.get('/', (req, res)=> {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/index', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
+// app.get('/index', (req, res)=> {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+//   });
 
 app.get('/joinus', (req, res)=> {
   res.sendFile(path.join(__dirname, '/joinus.htm'));
 });
 
 
+// app.post('/joinus', (req, res)=> {
+//   var data= new User(req.body);
+//   data.save().then(()=>{
+//     res.send(alert('Form Submitted Succesfully, Thank You!'))
+//   }).catch(()=>{
+//     res.status(404).send("data not saved")
+//   });
+// });
+
 app.post('/joinus', (req, res)=> {
   var data= new User(req.body);
-  data.save().then(()=>{
-
-    res.send(data)
-    
-  }).catch(()=>{
-    res.status(404).send(data)
+  data.save((err,doc)=>{
+    if(err){
+      res.send(err);
+    }
+    res.send("The document submitted succesfully")
   })
 });
-
 
 app.listen(port,()=>{
     console.log("app completed succesfully");
